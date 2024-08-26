aca5716e74ce4d1e04355a04bf848094f454e7d2
ANAN <ananshah328@gmail.com>
2024-08-26 12:14:44 +0000

    new changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33";
-console.log(typeof score);
+//console.log(typeof score);
 let scoreNumber = Number(score);
-console.log(typeof scoreNumber);
+//console.log(typeof scoreNumber);
 
 // "33" =>33
 // "33abc" => NaN
@@ -10,4 +10,12 @@ console.log(typeof scoreNumber);
 let isLoggedIn = 1; 
 let booleanIsLoggedIn = Boolean(isLoggedIn); // "" => false ; "someText" =>true
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+console.log("1" + 2); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+console.log(+true); //1
+console.log(+""); //0
+
+
---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+const accountId = 3453;
+let accountEmail = "ananshah328@gmail.com"; //scope limited
+var accountPassword = "04password";
+accountName = "anan";
+let accountState;
+console.table([
+    accountId,
+    accountEmail,
+    accountPassword,
+    accountName,
+    accountState
+]);
