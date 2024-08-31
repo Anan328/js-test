d2e2a8c4224b06333a1ebab4c3ceb0728d24237b
ANAN <ananshah328@gmail.com>
2024-08-31 04:54:43 +0000

    math file

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,23 @@
+const balance = new Number(100); //explicitly define data-type
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2)); //2 zeroes after decimal
+
+const num = 23.8966;
+// console.log(num.toPrecision(3));
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN')); //adds necessary commas
+
+//********************* MATHS ************************ 
+
+// console.log(Math.abs(-4)) //4
+// console.log(Math.round(4.8)) //ceil and floor also available
+// console.log(Math.min(4,3,6,7,8)) 
+for(var i=0;i<6;i++){
+    // console.log(Math.floor(Math.random()*6)+1);
+}
+
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random()*(max-min)+1)+min);
+
