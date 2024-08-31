21d7bef8e84a892a063bfc459e548f4384f22c50
ANAN <ananshah328@gmail.com>
2024-08-31 10:52:17 +0000

    functions and objects

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -3,4 +3,30 @@ function add(a,b){
 }
 
 const result = add(3,5);
-console.log(result);
+// console.log(result);
+
+function calculateCartPrice(...num1){ // here its is called rest operator
+    return num1;
+}
+function calculateCartPrice1(val1,val2,...num1){ 
+    const result = [val1,val2,num1]
+    return result;
+}
+
+// console.log(calculateCartPrice(100,200,300,700));
+// console.log(calculateCartPrice1(100,200,300,700));
+
+const user = {
+    name:"test",
+    id:1
+}
+
+function handleObject(obj){
+    console.log(`username is ${obj.name} and id is ${obj.id}`);
+    
+}
+// handleObject(user);
+handleObject({
+    name:"test",
+    id:2
+})
