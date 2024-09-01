b56181e58ebdb210533e776c36bbe5235c11b386
ANAN <ananshah328@gmail.com>
2024-09-01 15:52:21 +0000

    maps and loops

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,41 @@
 const coding = ['js','swift','node','react']
+
+// coding.map(code=>{
+//     console.log(code); 
+// })
+// coding.forEach((code)=>{
+//     console.log(code); 
+// })
+// coding.forEach(function (code){
+//     console.log(code); 
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe);
+
+coding.forEach((code,index,arr)=>{
+    //console.log(code,index,arr); 
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach(mycode=>{
+    console.log(`${mycode.languageName} : ${mycode.languageFileName}`); 
+})
