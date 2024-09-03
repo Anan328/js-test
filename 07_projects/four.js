babdf5b6f0672f04a589524489b59d0da69c305f
ANAN <ananshah328@gmail.com>
2024-09-03 12:55:20 +0000

    bugfixes

--- 07_projects/four.js
+++ 07_projects/four.js
@@ -6,39 +6,50 @@ const hint = document.getElementsByClassName('lowOrHi')[0];
 const random = Math.floor(Math.random()*100+1);
 console.log(random);
 const guessList = [];
+
+const correctGuess = (text)=>{
+    result.innerHTML = 'Who hoo!! correct guess';
+    result.style.backgroundColor = "green";
+    previousGuess.innerHTML = text;
+    setTimeout(function (){
+      alert("Congratuations, You won!!, play again");
+      window.location.reload();
+    },100)
+}
+
+const wrongGuess = (newResult,text)=>{
+    if(text < random){
+    hint.innerHTML = "Number is too low!!"
+    }else{
+    hint.innerHTML = "Number is too high!!"
+    }
+    lastResult.innerHTML = String(newResult);
+    result.innerHTML = 'Ah auu!! wrong guess';
+    result.style.backgroundColor = "red";
+    guessList.push(text)
+    previousGuess.innerHTML = guessList;
+}
+
+const remainingGuesses = (newResult)=>{
+  if(newResult==0){
+    alert("You lost all the guesses!!, play again");
+    result.innerHTML = 'You lost all the guesses!!';
+    result.style.backgroundColor = "red";
+    submit.disabled = true;
+    window.location.reload();
+  }
+}
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
     const text = document.getElementById('guessField').value;
     let newResult = parseInt(lastResult.innerHTML);
     if(random == text && text!=""){
-      result.innerHTML = 'Who hoo!! correct guess';
-      result.style.backgroundColor = "green";
-      previousGuess.innerHTML = text;
-      setTimeout(function (){
-        alert("Congratuations, You won!!, play again");
-       window.location.reload();
-      },100)
-      
+      correctGuess(text)
     }else if(random != text && text!=""){
-      if(text < random){
-            hint.innerHTML = "Number is too low!!"
-      }else{
-            hint.innerHTML = "Number is too high!!"
-      }
-      newResult=newResult-1;
+      newResult-=1;
       //console.log(newResult);
-      lastResult.innerHTML = String(newResult);
-      result.innerHTML = 'Ah auu!! wrong guess';
-      result.style.backgroundColor = "red";
-      guessList.push(text)
-      previousGuess.innerHTML = guessList;
-      if(newResult==0){
-        alert("You lost all the guesses!!, play again");
-        result.innerHTML = 'You lost all the guesses!!';
-        result.style.backgroundColor = "red";
-        submit.disabled = true;
-        window.location.reload();
-      }
+      remainingGuesses(newResult)
+      wrongGuess(newResult,text)
     }
 
 
