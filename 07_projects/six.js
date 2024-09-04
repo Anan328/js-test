866cc6728e6985c48771f6d264281f7f3f30cca9
ANAN <ananshah328@gmail.com>
2024-09-04 14:56:54 +0000

    more colors

--- 07_projects/six.js
+++ 07_projects/six.js
@@ -2,32 +2,16 @@ const body = document.querySelector('body');
 const start = document.querySelector('#start')
 const stop = document.querySelector('#stop')
 let colorFunction;
-const colors = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "purple",
-  "orange",
-  "pink",
-  "black",
-  "white",
-  "gray",
-  "brown",
-  "cyan",
-  "magenta",
-  "lime",
-  "teal",
-  "indigo",
-  "violet",
-  "gold",
-  "silver",
-  "navy"
-];
-
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 const changeColor = ()=>{
-  const index  = Math.floor(Math.random()*20)
-  body.style.backgroundColor=colors[index]
+  body.style.backgroundColor=randomColor();
 }
 start.addEventListener('click',()=>{
       colorFunction =  setInterval(changeColor,1000)
