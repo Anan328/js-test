e99fc52cabea233c492769abffc12e20e03bf8a2
ANAN <ananshah328@gmail.com>
2024-09-06 05:37:31 +0000

    more to know

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -3,6 +3,18 @@
 // IIFE is a function that runs as soon as it is defined.
 // Syntax: The function is enclosed in parentheses () to treat it as an expression, followed by another () to invoke it.
 
+// ******************  more to know ******************
+
+// IIFE (Immediately Invoked Function Expression)
+// - Creates a local scope immediately, preventing global scope pollution.
+// - Before ES6, `var` declarations leaked into the global scope, causing potential conflicts.
+// - IIFE helps encapsulate variables, making them local to the function and not affecting the global scope.
+// 
+// With the introduction of ES6:
+// - `let` and `const` provide block-level scope, reducing the need for IIFE in many cases.
+// - IIFE is still useful for encapsulation and protecting variables in complex scenarios.
+
+
 (function chai(){
     //named IIFE
     console.log('connected to database 1');
