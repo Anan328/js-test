aec41e36660fa525879fa7119c01922891fb51a6
ANAN <ananshah328@gmail.com>
2024-09-06 07:13:06 +0000

    behind the sceens

--- 10_OOP/myClasses.js
+++ 10_OOP/myClasses.js
@@ -12,8 +12,30 @@ class User{
         return `${this.username.toUpperCase()}`
     }
 }
-
+Object.prototype.hashPassword = function(){ //don't use arrow function
+    return `@#$%%$${this.password}djnencn`;
+}
 const userOne = new User('anan','example328@hmail.com','04example')
 console.log(userOne.encryptPassword());
 console.log(userOne.changeUsername());
- 
+console.log(userOne.hashPassword());
+
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
